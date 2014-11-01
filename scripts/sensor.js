e011e9d91225308e33ac0ddf342070f47278736a
Cezar Berea <berea.cezar@gmail.com>
2014-11-01 23:17:45 +0200

    Small improvements to sensor base class

--- scripts/sensor.js
+++ scripts/sensor.js
@@ -5,75 +5,96 @@ var
     blessed = require("blessed"),
     Sensor = require("../lib/protocol/sensor"),
     ADDR = "tcp://127.0.0.1:7654",
-    KEY = "foobar",
+    KEY = process.argv[2] || "foobar",
+    HELP = "<up>     - increase temp by 1\n" +
+           "<down>   - decrease temp by 1\n" +
+           "c        - connect sensor\n" +
+           "d        - disconnect sensor",
 
     crtTemperature = 20;
 
 var sensor = new Sensor({
   addr: ADDR,
-  key: KEY
+  key: KEY,
+  timeout: 2000,
+  info: {
+    name: "Dummy Sensor",
+    location: "Screen2"
+  }
 });
+
 sensor.connect();
+sensor.on("connect", function() {
+  log("sensor connected to broker");
+});
 
-//var screen = blessed.screen();
-//var stateBox = blessed.box({
-//  top: 0,
-//  left: 0,
-//  width: '25%',
-//  height: '25%',
-//  border: {
-//    type: 'line'
-//  },
-//  tags: true,
-//  label: "State",
-//  content: "foo"
-//});
-//var helpBox = blessed.box({
-//  top: 0,
-//  right: 0,
-//  width: "75%",
-//  height: "25%",
-//  border: {
-//    type: "line"
-//  },
-//  label: "Help",
-//  content: "help"
-//});
+sensor.on("disconnect", function() {
+  log("sensor disconnected from broker");
+});
 
-//var logBox = blessed.box({
-//  top: "25%",
-//  bottom: 0,
-//  width: "100%",
-//  height: "75%",
-//  border: {
-//    type: "line"
-//  },
-//  scrollable: true,
-//  label: "log",
-//  content: ""
-//});
+var screen = blessed.screen();
+var stateBox = blessed.box({
+  top: 0,
+  left: 0,
+  width: '25%',
+  height: '25%',
+  border: {
+    type: 'line'
+  },
+  tags: true,
+  label: "State",
+  content: ""
+});
+var helpBox = blessed.box({
+  top: 0,
+  right: 0,
+  width: "75%",
+  height: "25%",
+  border: {
+    type: "line"
+  },
+  label: "Help",
+  content: HELP
+});
 
-//screen.append(stateBox);
-//screen.append(helpBox);
-//screen.append(logBox);
-//screen.render();
-
-
-//screen.key(["up", "down"], function(ch, key) {
-//  var factor = 0;
-//  if (key.name === "up") {
-//    factor = 1;
-//  } else if (key.name === "down") {
-//    factor = -1;
-//  }
-
-//  setTemp(crtTemperature + factor);
-//});
+var logBox = blessed.box({
+  top: "25%",
+  bottom: 0,
+  width: "100%",
+  height: "75%",
+  border: {
+    type: "line"
+  },
+  scrollable: true,
+  label: "log",
+  content: ""
+});
 
 
-//screen.key(["C-c", "q"], function(ch, key) {
-//  return process.exit(0);
-//});
+screen.key(["up", "down"], function(ch, key) {
+  var factor = 0;
+  if (key.name === "up") {
+    factor = 1;
+  } else if (key.name === "down") {
+    factor = -1;
+  }
+
+  setTemp(crtTemperature + factor);
+});
+
+screen.key("c", function() { sensor.connect(); });
+screen.key("d", function() { sensor.disconnect(); });
+
+screen.key(["C-c", "q"], function(ch, key) {
+  return process.exit(0);
+});
+
+screen.append(stateBox);
+screen.append(helpBox);
+screen.append(logBox);
+screen.render();
+
+setTemp(20);
 
 function setTemp(temp) {
   crtTemperature = temp;
